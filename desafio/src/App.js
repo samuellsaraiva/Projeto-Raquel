6f6c12425ffb03169e38e6221ded2a4cbcc9991b
raqueleucaria <raqueleucaria@gmail.com>
2022-03-26 15:33:29 -0300

    Estilo do projeto
    Criação da estrutura/estilo geral do projeto, seguindo o layout do figma.

--- desafio/src/App.js
+++ desafio/src/App.js
@@ -1,9 +1,48 @@
+import './styles.css';
+import { FaFilter, FaInfoCircle}  from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Dashboard</h1>
-        <h3>Desafio Técnico Frontend</h3>
+    
+    <div className="container">
+      <div className="topBar"> {/* Ajeitar melhor */}</div> 
+    
+      <div>
+        <h1 className="title">Dashboard</h1>
+        <h2 className="subTitle">Desafio Técnico Frontend</h2>
+      </div>
+      
+      <button className="buttonSearch">
+        <FaFilter style={{marginRight: 8}} size={12} color="#FFF"/>
+        Filtrar
+      </button>
+      
+      
+      <main className="main">
+        <div className="barGraph">
+          <div className='titleGraph'>
+            Barras
+            <button className='info'> 
+              <FaInfoCircle size={15} color="#19D7AA"/>
+            </button>
+          </div>
+        
+
+        </div>
+
+        <div className="scatterGraph">
+        <div className='titleGraph'>
+            Scatter
+            <button className='info'> 
+              <FaInfoCircle size={15} color="#19D7AA"/>
+            </button>
+          </div>
+        
+
+        </div>
+      </main>
+    
+    
     </div>
   );
 }
